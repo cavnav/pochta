fedf4e7e509acab4e6f0deee2eb625496d82962c
Dmitry Goryachev <cavnav@gmail.com>
2020-07-30 12:16:46 -0700

    Initial commit

---
+++ src/Components/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from '../../logo.svg';
+import { UserList } from '../';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <UserList />
+    </div>
+  );
+}
+
+export default App;
